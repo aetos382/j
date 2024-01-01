12ca38108482fc3af78bd346b8c63d3f56b614a0
aetos <aetos382@users.noreply.github.com>
2024-01-01 23:46:09 +0900

    remove shell-history feature
    remoteUser を指定すると動かない

--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/bash
 
-# Install the version of Bundler.
-if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null; then
-    cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
-fi
+printenv
 
-# If there's a Gemfile, then run `bundle install`
-# It's assumed that the Gemfile will install Jekyll too
-if [ -f Gemfile ]; then
-    bundle install
-fi
+export HISTFILE=/dc/shellhistory/.bash_history
+export PROMPT_COMMAND='history -a'
